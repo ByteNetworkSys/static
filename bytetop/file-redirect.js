8494263d005f2a53be67d86ade33363fe43fa52c
Mixxer <80760007+MEEBLOX@users.noreply.github.com>
2022-12-21 07:00:21 -0500

    Create file-redirect.js

---
+++ bytetop/file-redirect.js
@@ -0,0 +1,33 @@
+loadScript("https://static90fba.mixxer.repl.co/bytetop/roles/roles.js")
+
+loadScript("https://static90fba.mixxer.repl.co/bytetop/roles/roleUsers.js")
+
+// Mark for history purposes (Dec 10, 11:02 EST) You fixed ByteTop with @Siri!
+
+loadScript("https://static90fba.mixxer.repl.co/bytetop/roles/generateRoles.js")
+
+
+
+function removeThemes() {
+  localStorage.removeItem("theme")
+}
+// it worked :D
+function setThemes() {
+if (localStorage.getItem("theme") == "PurpOled") {
+            premiumDisplay("PurpOled")
+          }
+
+if (localStorage.getItem("theme") == "BlueOled") {
+            premiumDisplay("BlueOled")
+          }
+if (localStorage.getItem("theme") == "pride") {
+            premiumDisplay("Pride")
+          }
+}
+// THE PURPOLED THEME NO WROKKKK!!!?!?!?!? yeah I think I fixed it though with this O_O though now wene
+// you fixed it | yes. thing is, we now need to set theme to blank when you click the other theme things
+// That already works.
+// well 
+// nvm | it was bugging out lmao
+
+// it worked
