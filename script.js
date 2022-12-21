365eaba2ebcfb7483920722b3dd466fb3fc5d13e
Mixxer <80760007+MEEBLOX@users.noreply.github.com>
2022-12-21 07:04:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function ezSwal() {
     Swal.fire({
     title: "System message",
-    text: "You've reached the static file system for @Mixxer's account! Please visit a valid static file under one of the directories.",
+    text: "You've reached the static file system for ByteNetwork! Please visit a valid static file under one of the directories.",
     icon: "info",
     confirmButtonText: "Got it!"
   })
