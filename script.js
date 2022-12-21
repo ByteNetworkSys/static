cecc358a446bd24908e952996f3437e63594ffa5
Mixxer <80760007+MEEBLOX@users.noreply.github.com>
2022-12-21 07:01:34 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function ezSwal() {
+    Swal.fire({
+    title: "System message",
+    text: "You've reached the static file system for @Mixxer's account! Please visit a valid static file under one of the directories.",
+    icon: "info",
+    confirmButtonText: "Got it!"
+  })
+}
+
+// check bytetop/roles
